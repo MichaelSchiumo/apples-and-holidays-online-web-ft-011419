b4b9ec85ba7dd531a545f7f710d4281eaf9512b1
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-16 21:45:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,6 +91,7 @@ def all_holidays_with_bbq(holiday_hash)
     holiday_hash.each do |holiday, supplies|
       supplies.each do |supply|
         if supply == 'BBQ'
+          return :holiday
 end
 
  
